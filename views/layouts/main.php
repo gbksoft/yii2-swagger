ca442421093bfdffde2415e009f6682745931f3f
Hryhorii Furletov <littlefuntik@gmail.com>
2016-02-01 18:23:38 +0200

    JS on Ready document

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,5 @@
 <?php
+use yii\web\View;
 use yii\helpers\Html;
 use gbksoft\modules\swagger\SwaggerAsset;
 
@@ -6,6 +7,77 @@ use gbksoft\modules\swagger\SwaggerAsset;
 /* @var $content string */
 
 SwaggerAsset::register($this);
+$js = <<<JS
+    var url = window.location.search.match(/url=([^&]+)/);
+    if (url && url.length > 1) {
+      url = decodeURIComponent(url[1]);
+    } else {
+      url = "{$this->context->module->swaggerPath}";
+    }
+
+    // Pre load translate...
+    if(window.SwaggerTranslator) {
+      window.SwaggerTranslator.translate();
+    }
+    window.swaggerUi = new SwaggerUi({
+      url: url,
+      dom_id: "swagger-ui-container",
+      supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+      onComplete: function(swaggerApi, swaggerUi){
+        if(typeof initOAuth == "function") {
+          initOAuth({
+            clientId: "your-client-id",
+            clientSecret: "your-client-secret-if-required",
+            realm: "your-realms",
+            appName: "your-app-name",
+            scopeSeparator: ",",
+            additionalQueryStringParams: {}
+          });
+        }
+
+        if(window.SwaggerTranslator) {
+          window.SwaggerTranslator.translate();
+        }
+
+        $('pre code').each(function(i, e) {
+          hljs.highlightBlock(e)
+        });
+
+        addApiKeyAuthorization();
+
+
+      },
+      onFailure: function(data) {
+        log("Unable to Load SwaggerUI");
+      },
+      docExpansion: "none",
+      jsonEditor: false,
+      apisSorter: "alpha",
+      defaultModelRendering: 'schema',
+      showRequestHeaders: true,
+      /*,validatorUrl: "http://localhost:8002"*/
+    });
+
+    function addApiKeyAuthorization(){
+      var key = encodeURIComponent( $('#input_apiKey')[0].value );
+      if(key && key.trim() != "") {
+          var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization( "Authorization", "Bearer " + key, "header" );
+          window.swaggerUi.api.clientAuthorizations.add( "bearer", apiKeyAuth );
+          log( "Set bearer token: " + key );
+      }
+    }
+
+    $('#input_apiKey').change(addApiKeyAuthorization);
+
+    window.swaggerUi.load();
+
+    function log() {
+      if ('console' in window) {
+        console.log.apply(console, arguments);
+      }
+    }
+JS;
+$this->registerJs($js, View::POS_READY);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -16,78 +88,6 @@ SwaggerAsset::register($this);
   <?= Html::csrfMetaTags() ?>
   <title><?= Html::encode($this->title) ?></title>
   <?php $this->head() ?>
-  <script type="text/javascript">
-    $(function () {
-      var url = window.location.search.match(/url=([^&]+)/);
-      if (url && url.length > 1) {
-        url = decodeURIComponent(url[1]);
-      } else {
-        url = "<?= $this->context->module->swaggerPath ?>";
-      }
-
-      // Pre load translate...
-      if(window.SwaggerTranslator) {
-        window.SwaggerTranslator.translate();
-      }
-      window.swaggerUi = new SwaggerUi({
-        url: url,
-        dom_id: "swagger-ui-container",
-        supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-        onComplete: function(swaggerApi, swaggerUi){
-          if(typeof initOAuth == "function") {
-            initOAuth({
-              clientId: "your-client-id",
-              clientSecret: "your-client-secret-if-required",
-              realm: "your-realms",
-              appName: "your-app-name",
-              scopeSeparator: ",",
-              additionalQueryStringParams: {}
-            });
-          }
-
-          if(window.SwaggerTranslator) {
-            window.SwaggerTranslator.translate();
-          }
-
-          $('pre code').each(function(i, e) {
-            hljs.highlightBlock(e)
-          });
-
-          addApiKeyAuthorization();
-
-
-        },
-        onFailure: function(data) {
-          log("Unable to Load SwaggerUI");
-        },
-        docExpansion: "none",
-        jsonEditor: false,
-        apisSorter: "alpha",
-        defaultModelRendering: 'schema',
-        showRequestHeaders: true,
-        /*,validatorUrl: "http://localhost:8002"*/
-      });
-
-      function addApiKeyAuthorization(){
-        var key = encodeURIComponent( $('#input_apiKey')[0].value );
-        if(key && key.trim() != "") {
-            var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization( "Authorization", "Bearer " + key, "header" );
-            window.swaggerUi.api.clientAuthorizations.add( "bearer", apiKeyAuth );
-            log( "Set bearer token: " + key );
-        }
-      }
-
-      $('#input_apiKey').change(addApiKeyAuthorization);
-
-      window.swaggerUi.load();
-
-      function log() {
-        if ('console' in window) {
-          console.log.apply(console, arguments);
-        }
-      }
-  });
-  </script>
 </head>
 <body class="swagger-section">
 <?php $this->beginBody() ?>
