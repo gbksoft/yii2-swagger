ae891dd953e8b44d92f162dcda2b3205470044d2
Hryhorii Furletov <littlefuntik@gmail.com>
2016-02-01 18:14:18 +0200

    Get module in layout Fix #4

--- views/default/index.php
+++ views/default/index.php
@@ -1,1 +1,19 @@
-test index
+<div id='header'>
+<div class="swagger-ui-wrap">
+  <a id="logo" href="http://swagger.io">swagger</a>
+  <form id='api_selector'>
+      <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
+    <div class='input'><input placeholder="Authorization" id="input_apiKey" name="apiKey" type="text"/></div>
+    <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
+  </form>
+</div>
+</div>
+
+<div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
+<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
+
+<!-- API history block -->
+<div class="swagger-ui-wrap log-wrap">
+  <h2>Short API History &mdash; last 10 swagger commits</h2>
+  <div id="apihistory" class="wrap-log"></div>
+</div> <!-- End of API history block -->
